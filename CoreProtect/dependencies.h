7196c861e3adf53acb6d897e24c805fa62b88df4
Константин Цай <84087988+KocTu4eK@users.noreply.github.com>
2022-01-21 10:00:39 +0600

    Update dependencies.h

--- CoreProtect/dependencies.h
+++ CoreProtect/dependencies.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "pch.h"
 
-#define R tr("COLOR_RESTORE")
-#define B tr("COLOR_BLUE")
-#define G tr("COLOR_GREY")
+#define CR tr("COLOR_RESTORE")
+#define CB tr("COLOR_BLUE")
+#define CG tr("COLOR_GREY")
 
 enum CommandParameterOption;
 static Logger logger;
