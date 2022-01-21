55dbe99e5d2b599fbe9f70b721a469217f9e69ce
Константин Цай <84087988+KocTu4eK@users.noreply.github.com>
2022-01-21 10:06:29 +0600

    Update place_block.cpp

--- CoreProtect/listener/block/place_block.cpp
+++ CoreProtect/listener/block/place_block.cpp
@@ -7,15 +7,16 @@ bool Listener::playerPlaceBlock(Event::PlayerPlaceBlockEvent ev)
 	Player* pl = ev.mPlayer;
 	Block* bl = ev.mBlockInstance.getBlock();
 	BlockPos pos = ev.mBlockInstance.getPosition();
-	std::string coords = "x" + std::to_string(pos.x) + "/y" + std::to_string(pos.y) + "/z" + std::to_string(pos.z); // «x0/y0/z0» (co_block.xyz)
+	std::string coords = "x" + std::to_string(pos.x) + "/y" + std::to_string(pos.y) + "/z" + std::to_string(pos.z);
 	int isInspector = -1;
+	int pageCount = -1;
 	std::string userId;
 
-	Utils::sqlExecute(std::string("SELECT * FROM co_user WHERE nick = '" + pl->getName() + "';").c_str(), [&](SQLite::Statement& sm) {
-		sm.executeStep();
-		isInspector = sm.getColumn("inspect").getInt();
-		userId = sm.getColumn("id").getString();
-	});
+	Utils::sqlExecute(std::string("SELECT * FROM co_user WHERE nick = '" + pl->getName() + "';").c_str(), [&isInspector, &userId](SQLite::Statement& user) {
+		user.executeStep();
+		isInspector = user.getColumn("inspect").getInt();
+		userId = user.getColumn("id").getString();
+		});
 
 	if (!isInspector)
 	{
@@ -24,60 +25,42 @@ bool Listener::playerPlaceBlock(Event::PlayerPlaceBlockEvent ev)
 	}
 	else
 	{
-		Utils::sqlExecute(std::string("UPDATE co_user SET l_xyz = '-', l_id = -1, l_idn = -1, l_pages = -1, l_page = 1 WHERE nick = '" + pl->getName() + "';").c_str());
-		Utils::sqlExecute(std::string("SELECT id FROM co_block WHERE xyz = '" + coords + "' ORDER BY id DESC;").c_str(), [&](SQLite::Statement& sm) {
-			if (sm.executeStep())
+		Utils::sqlExecute(std::string("SELECT COUNT(user) FROM co_block WHERE xyz = '" + coords + "';").c_str(), [&pageCount](SQLite::Statement& count) {
+			count.executeStep();
+			pageCount = Utils::pagesCalculate(count.getColumn("COUNT(user)").getInt());
+			});
+
+		Utils::sqlExecute(std::string("SELECT * FROM co_block WHERE xyz = '" + coords + "' ORDER BY time DESC LIMIT 0, 7;").c_str(), [&](SQLite::Statement& block) {
+			if (block.executeStep())
 			{
-				int descId = sm.getColumn("id").getInt(); // first received id (descending)
-				int rowCount = 0;
-				do rowCount++; while (sm.executeStep());
-				int pageCount = Utils::pagesCalculate(rowCount); // 25 rows = 4 pages (3 pages + 1 (remainder))
-				
-				Utils::sqlExecute(std::string("SELECT * FROM co_block WHERE xyz = '" + coords + "' AND id <= " + std::to_string(descId) + " ORDER BY id DESC LIMIT 7;").c_str(), [&](SQLite::Statement& sm) {
-					if (pageCount == 1)
-					{
-						pl->sendText(tr("INSPECT_HEADER") + coords + ")\n"); // ----- CoreProtect ----- (x0/y0/z0)
-						while (sm.executeStep())
-						{
-							Utils::sqlExecute(std::string("SELECT nick FROM co_user WHERE id = " + sm.getColumn("user").getString() + ";").c_str(), [&](SQLite::Statement& smUser) {
-								smUser.executeStep();
-								if (!sm.getColumn("action").getInt())
-								{				                            // 0.00/h ago — KocTu4eK removed Diamond Block.
-									pl->sendText(G + "0.00/h ago" + R + " - " + B + smUser.getColumn("nick").getString() + R + " removed " + B + sm.getColumn("block").getString() + R + ".");
-								}
-								else
-								{				                            // 0.00/h ago — KocTu4eK placed Netherite Block.
-									pl->sendText(G + "0.00/h ago" + R + " - " + B + smUser.getColumn("nick").getString() + R + " placed " + B + sm.getColumn("block").getString() + R + ".");
-								}
+				std::chrono::system_clock sc;
+				if (pageCount == 1)
+				{
+					pl->sendText(tr("INSPECT_HEADER") + coords + ")\n");
+					do {
+						Utils::sqlExecute(std::string("SELECT nick FROM co_user WHERE id = " + block.getColumn("user").getString() + ";").c_str(), [&block, &pl, &sc](SQLite::Statement& userNick) {
+							userNick.executeStep();
+							if (!block.getColumn("action").getInt()) pl->sendText(CG + Utils::timestampFormat(sc, block.getColumn("time").getInt()) + " ago" + CR + " - " + CB + userNick.getColumn("nick").getString() + CR + " removed " + CB + block.getColumn("block").getString() + CR + ".");
+							else pl->sendText(CG + Utils::timestampFormat(sc, block.getColumn("time").getInt()) + " ago" + CR + " - " + CB + userNick.getColumn("nick").getString() + CR + " placed " + CB + block.getColumn("block").getString() + CR + ".");
 							});
-						}
-					}
-					else if (pageCount > 1)
-					{
-						int nowId = -1;
-						pl->sendText(tr("INSPECT_HEADER") + coords + ")\n"); // ----- CoreProtect ----- (x0/y0/z0)
-						while (sm.executeStep())
-						{
-							Utils::sqlExecute(std::string("SELECT nick FROM co_user WHERE id = " + sm.getColumn("user").getString() + ";").c_str(), [&](SQLite::Statement& smUser) {
-								smUser.executeStep();
-								if (!sm.getColumn("action").getInt())
-								{				                            // 0.00/h ago — KocTu4eK removed Diamond Block.
-									pl->sendText(G + "0.00/h ago" + R + " - " + B + smUser.getColumn("nick").getString() + R + " removed " + B + sm.getColumn("block").getString() + R + ".");
-								}
-								else
-								{				                            // 0.00/h ago — KocTu4eK placed Netherite Block.
-									pl->sendText(G + "0.00/h ago" + R + " - " + B + smUser.getColumn("nick").getString() + R + " placed " + B + sm.getColumn("block").getString() + R + ".");
-								}
-								nowId = sm.getColumn("id").getInt();
+					} while (block.executeStep());
+				}
+				else if (pageCount > 1)
+				{
+					pl->sendText(tr("INSPECT_HEADER") + coords + ")\n");
+					do {
+						Utils::sqlExecute(std::string("SELECT nick FROM co_user WHERE id = " + block.getColumn("user").getString() + ";").c_str(), [&block, &pl, &sc](SQLite::Statement& userNick) {
+							userNick.executeStep();
+							if (!block.getColumn("action").getInt()) pl->sendText(CG + Utils::timestampFormat(sc, block.getColumn("time").getInt()) + " ago" + CR + " - " + CB + userNick.getColumn("nick").getString() + CR + " removed " + CB + block.getColumn("block").getString() + CR + ".");
+							else pl->sendText(CG + Utils::timestampFormat(sc, block.getColumn("time").getInt()) + " ago" + CR + " - " + CB + userNick.getColumn("nick").getString() + CR + " placed " + CB + block.getColumn("block").getString() + CR + ".");
 							});
-						}
-						Utils::sqlExecute(std::string("UPDATE co_user SET l_xyz = '" + coords + "', l_id = " + std::to_string(descId) + ", l_idn = " + std::to_string(nowId) + ", l_pages = " + std::to_string(pageCount) + ", l_pages = " + std::to_string(pageCount) + " WHERE nick = '" + pl->getName() + "'; ").c_str());
-						pl->sendText(tr("LOOKUP_VIEW_PAGE", "1/" + std::to_string(pageCount) + tr("SYMBOL_ARROW_RIGHT"), "/co l <page>")); // ————— Page 1/1 ►. View older typing "/co l <page>".
-					};
-				});
+					} while (block.executeStep());
+					Utils::sqlExecute(std::string("UPDATE co_user SET l_xyz = '" + coords + "' WHERE nick = '" + pl->getName() + "'; ").c_str());
+					pl->sendText(tr("LOOKUP_VIEW_PAGE", "1/" + std::to_string(pageCount) + tr("SYMBOL_ARROW_RIGHT"), "/co l <page>"));
+				};
 			}
 			else pl->sendText(tr("NO_DATA_LOCATION", "data"));
-		});
+			});
 		return false;
 	}
 }
