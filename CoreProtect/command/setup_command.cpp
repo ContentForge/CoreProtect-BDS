3dd7f9fe9e879e53d50e94856fd9d9a460b40848
Константин Цай <84087988+KocTu4eK@users.noreply.github.com>
2022-01-21 10:09:14 +0600

    Update setup_command.cpp

--- CoreProtect/command/setup_command.cpp
+++ CoreProtect/command/setup_command.cpp
@@ -4,35 +4,49 @@
 
 void CoreProtect::setup(CommandRegistry* r)
 {
-	using RegisterCommandHelper::makeMandatory;
-	using RegisterCommandHelper::makeOptional;
+    using RegisterCommandHelper::makeMandatory;
+    using RegisterCommandHelper::makeOptional;
 
-	r->registerCommand("coreprotect", "CoreProtect", CommandPermissionLevel::Any, DEFAULT_COMMAND_FLAGS); // coreprotect
-	r->registerAlias("coreprotect", "co");                                                                // co (alias)
+    r->registerCommand("coreprotect", "CoreProtect", CommandPermissionLevel::Any, DEFAULT_COMMAND_FLAGS); // coreprotect
+    r->registerAlias("coreprotect", "co");                                                                // co (alias)
 
-	r->addEnum<Option>("help", { {"help", Option::HELP} });                           // help
-	r->addSoftEnum("helpParam", { {"help"}, {"inspect"}, {"rollback"}, {"lookup"} }); // help param
-	r->addEnum<Option>("inspect", { {"inspect", Option::INSPECT} });                  // inspect
-	r->addEnum<Option>("i", { {"i", Option::I} });                                    // i (inspect)
-	r->addEnum<Option>("rollback", { {"rollback", Option::ROLLBACK} });               // rollback
-	r->addEnum<Option>("lookup", { {"lookup", Option::LOOKUP} });                     // lookup
-	r->addEnum<Option>("l", { {"l", Option::L} });                                    // l (list)
+    ///////////////////////////////////////////////////////////////////////// Options //////////////////////////////////////////////////////////////////////
+    r->addEnum<Option>("help", { {"help", Option::HELP} });                                                                                                  // help option enum
+    r->addEnum<Option>("inspect", { {"inspect", Option::INSPECT} });                                                                                         // inspect option enum
+    r->addEnum<Option>("i", { {"i", Option::INSPECT} });                                                                                                     // i (inspect) option enum
+    r->addEnum<Option>("rollback", { {"rollback", Option::ROLLBACK} });                                                                                      // rollback option enum
+    r->addEnum<Option>("lookup", { {"lookup", Option::LOOKUP} });                                                                                            // lookup option enum
+    r->addEnum<Option>("l", { {"l", Option::LOOKUP} });                                                                                                      // l (lookup) option enum
+    auto helpOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "help", "help").addOptions((CommandParameterOption)1);             // help option
+    auto inspectOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "inspect", "inspect").addOptions((CommandParameterOption)1);    // inspect option
+    auto iOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "i", "i").addOptions((CommandParameterOption)1);                      // i (inspect) option
+    auto rollbackOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "rollback", "rollback").addOptions((CommandParameterOption)1); // rollback option
+    auto lookupOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "lookup", "lookup").addOptions((CommandParameterOption)1);       // lookup option
+    auto lOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "l", "l").addOptions((CommandParameterOption)1);                      // l (lookup) option
 
-	auto helpOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "help", "help").addOptions((CommandParameterOption)1);			 // help
-	auto helpParam = makeOptional<CommandParameterDataType::SOFT_ENUM>(&CoreProtect::helpParam, "param", "helpParam", &CoreProtect::isSetHelpParam);         // help param
-	auto inspectOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "inspect", "inspect").addOptions((CommandParameterOption)1);    // inspect
-	auto iOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "i", "i").addOptions((CommandParameterOption)1);						 // i (inspect)
-	auto rollbackOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "rollback", "rollback").addOptions((CommandParameterOption)1); // rollback
-	auto lookupOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "lookup", "lookup").addOptions((CommandParameterOption)1);       // lookup
-	auto lOption = makeMandatory<CommandParameterDataType::ENUM>(&CoreProtect::option, "l", "l").addOptions((CommandParameterOption)1);                      // l (list)
-	auto lPageParam = makeMandatory(&CoreProtect::lookupPage, "page");																						 // lookup page
-	auto rollbackTimeParam = makeMandatory(&CoreProtect::rollbackTime, "time");																				 // rollback time
-	auto rollbackActionParam = makeOptional(&CoreProtect::rollbackAction, "action", &CoreProtect::isSetRollbackAction);									     // rollback action
+    ///////////////////////////////////////////////////////// Lookup & Rollback parameters ////////////////////////////////////////////////////////
+    r->addSoftEnum("action", { {"+block"}, {"-block"}, {"chat"}, {"kill"} });                                                                       // action enum
+    r->addSoftEnum("include", { {"stone"}, {"wood"}, {"glass"}, {"wool"} });                                                                        // include enum
+    r->addSoftEnum("exclude", { {"stone"}, {"wood"}, {"glass"}, {"wool"} });                                                                        // exclude enum
+    auto userParam = makeMandatory(&CoreProtect::user, "user");                                                                                     // user <>
+    auto timeParam = makeMandatory(&CoreProtect::time, "time");                                                                                     // time <>
+    auto radiusParam = makeMandatory(&CoreProtect::radius, "radius");                                                                               // radius <>
+    auto actionParam = makeOptional<CommandParameterDataType::SOFT_ENUM>(&CoreProtect::action, "action", "action", &CoreProtect::isSetAction);      // action []
+    auto includeParam = makeOptional<CommandParameterDataType::SOFT_ENUM>(&CoreProtect::include, "include", "include", &CoreProtect::isSetInclude); // include []
+    auto excludeParam = makeOptional<CommandParameterDataType::SOFT_ENUM>(&CoreProtect::exclude, "exclude", "exclude", &CoreProtect::isSetExclude); // exclude []
 
-	r->registerOverload<CoreProtect>("co", helpOption, helpParam);                                  // co help [param: string]
-	r->registerOverload<CoreProtect>("co", inspectOption);                                          // co inspect
-	r->registerOverload<CoreProtect>("co", iOption);                                                // co i
-	r->registerOverload<CoreProtect>("co", rollbackOption, rollbackTimeParam, rollbackActionParam); // co rollback <time: int> [action: string]
-	r->registerOverload<CoreProtect>("co", lookupOption);                                           // co lookup
-	r->registerOverload<CoreProtect>("co", lOption, lPageParam);                                    // co l <page: int>
+    ////////////////////////////////////////////////////////////////////// Other ///////////////////////////////////////////////////////////////////
+    r->addSoftEnum("helpParam", { {"help"}, {"inspect"}, {"rollback"}, {"lookup"} });                                                                // help enum
+    auto helpParam = makeOptional<CommandParameterDataType::SOFT_ENUM>(&CoreProtect::helpParam, "param", "helpParam", &CoreProtect::isSetHelpParam); // help param []
+    auto lookupPageParam = makeMandatory(&CoreProtect::lookupPage, "page", &CoreProtect::isSetLookupPage);                                           // lookup page param <>
+
+    ///////////////////////////////////////////////////////// Command Overloads ///////////////////////////////////////////////////////
+    r->registerOverload<CoreProtect>("co", helpOption, helpParam);                                                                      // co help [param:string]
+    r->registerOverload<CoreProtect>("co", inspectOption);                                                                              // co inspect
+    r->registerOverload<CoreProtect>("co", iOption);                                                                                    // co i
+    r->registerOverload<CoreProtect>("co", rollbackOption, userParam, timeParam, radiusParam, actionParam, includeParam, excludeParam); // co rollback <user:target> <time:string> <radius:int> [action:string] [include:string] [exclude:string]
+    r->registerOverload<CoreProtect>("co", lookupOption, lookupPageParam);                                                              // co lookup <page: int>
+    r->registerOverload<CoreProtect>("co", lookupOption, userParam, timeParam, radiusParam, actionParam, includeParam, excludeParam);   // co lookup <user:target> <time:string> <radius:int> [action:string] [include:string] [exclude:string]
+    r->registerOverload<CoreProtect>("co", lOption, lookupPageParam);                                                                   // co l <page: int>
+    r->registerOverload<CoreProtect>("co", lOption, userParam, timeParam, radiusParam, actionParam, includeParam, excludeParam);        // co l <user:target> <time:string> <radius:int> [action:string] [include:string] [exclude:string]
 }
