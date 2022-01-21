372ff4e2bc25586cfc1e4133812eb6bdf93cd66e
Константин Цай <84087988+KocTu4eK@users.noreply.github.com>
2022-01-21 10:00:13 +0600

    Update plugin.cpp

--- CoreProtect/plugin.cpp
+++ CoreProtect/plugin.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "dependencies.h"
 #include "listener.h"
 #include "command.h"
@@ -10,8 +10,8 @@ void plugin()
 	CreateDirs("./plugins/CoreProtect/");
 	Translation::load("./plugins/CoreProtect/language.json");
 
-	Utils::sqlExecute("CREATE TABLE IF NOT EXISTS co_block(id INTEGER PRIMARY KEY AUTOINCREMENT, user INT, xyz VARCHAR(255), block VARCHAR(255), action INT, time INT);");
-	Utils::sqlExecute("CREATE TABLE IF NOT EXISTS co_user(id INTEGER PRIMARY KEY AUTOINCREMENT, nick VARCHAR(255), inspect INT DEFAULT 0, l_xyz VARCHAR(255) DEFAULT '-', l_id INT DEFAULT -1, l_idn INT DEFAULT -1, l_pages INT DEFAULT -1, l_page INT DEFAULT 1);");
+	Utils::sqlExecute("CREATE TABLE IF NOT EXISTS co_block(time INT DEFAULT(strftime('%s', 'now')), user INT, wid INT, xyz VARCHAR(255), block VARCHAR(255), action INT);");
+	Utils::sqlExecute("CREATE TABLE IF NOT EXISTS co_user(id INTEGER PRIMARY KEY AUTOINCREMENT, nick VARCHAR(255), inspect INT DEFAULT 0, l_xyz VARCHAR(255) DEFAULT '-');");
 
 	Event::PlayerPreJoinEvent::subscribe(Listener::playerPreJoin);           // Listener/PlayerPreJoin
 	Event::PlayerPlaceBlockEvent::subscribe(Listener::playerPlaceBlock);     // Listener/Block/PlayerPlaceBlock
